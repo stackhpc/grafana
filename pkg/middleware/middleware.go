94553d5edc047f3598056922f1a6c2b3e887d306
Darren Hague <d.hague@sap.com>
2016-06-21 14:42:38 +0100

    Squashed set of Darren's commits
    Log the URL when calling a datasource from the proxy
    
    Fixes https://github.com/twc-openstack/grafana/issues/5
    
    Configuration options:
    cookie_credentials - set to true to use cookie instead of session for storing Keystone password
    credential_aes_key - 32-char encryption key. If set, this key is used to encrypt/decrypt the stored Keystone password
    
    Multi-domain login
    
    Fix: allow users with cross-domain role assignments to log in
    
    (cherry picked from commit 66316d7)
    
    Case sensitive login fix
    
    If the backend Keystone is case-insensitive (perhaps using AD for authentication)
    then we could end up with several users in Grafana, one for each combination of
     upper & lowercase chars in the username. This fix always uses the username returned
     in the Keystone response as the username for Grafana, regardless of the case used
     in the login screen.
    
    (cherry picked from commit 1b8b6f9)
    
    Fix bug in "Case sensitive login fix"
    
    (cherry picked from commit 27a5f06)
    
    Formatting updates - "go fmt" applied
    
    Check length of ciphertext *before* decoding it
    
    (cherry picked from commit 0e54407)

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -263,4 +263,6 @@ func AddDefaultResponseHeaders() macaron.Handler {
 			ctx.Resp.Header().Add("Expires", "-1")
 		}
 	}
+func IsSecure(ctx *Context) bool {
+	return (ctx.Req.TLS != nil) || (ctx.Req.Header.Get("X-Forwarded-Proto") == "https")
 }
