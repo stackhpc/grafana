5d28f54dfbdefd326d1fba6250a4cb40b3b60e36
Johannes Grassler <jgr-github@btw23.de>
2018-01-18 06:24:18 +0100

    Adapt to updated monasca-grafana-plugin (#1)
    This commit adds (backwards compatible) support for the new
    auth mechanism selection mechanism introduced in
    monasca-grafana-datasource by
    
    https://github.com/openstack/monasca-grafana-datasource/commit/bb5178406c03d37a957b64ef59c61ecdec504fc3

--- pkg/api/dataproxy.go
+++ pkg/api/dataproxy.go
@@ -55,7 +55,13 @@ func (hs *HttpServer) ProxyDataSourceRequest(c *middleware.Context) {
 		return
 	}
 
-	keystoneAuth := ds.JsonData.Get("keystoneAuth").MustBool()
+	keystoneAuth := ds.JsonData.Get("keystoneAuth").MustBool(false)
+
+	// Try authType (new setting name) as well if keystoneAuth is missing/false
+	if !keystoneAuth {
+		keystoneAuth = (ds.JsonData.Get("authMode").MustString("") == "Keystone")
+	}
+
 	if keystoneAuth {
 		token, err := keystone.GetToken(c)
 		if err != nil {
