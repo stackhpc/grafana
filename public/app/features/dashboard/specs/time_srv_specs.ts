84b05253b01573269ae10d8803117172446a47f7
Isaac Prior <isaac@stackhpc.com>
2019-06-21 14:08:41 +0100

    Fix time_srv_spec import error

--- public/app/features/dashboard/specs/time_srv_specs.ts
+++ public/app/features/dashboard/specs/time_srv_specs.ts
@@ -2,7 +2,7 @@ import {describe, beforeEach, it, sinon, expect, angularMocks} from 'test/lib/co
 
 import helpers from 'test/specs/helpers';
 import _ from 'lodash';
-import TimeSrv from '../time_srv';
+import '../time_srv';
 import moment from 'moment';
 
 describe('timeSrv', function() {
